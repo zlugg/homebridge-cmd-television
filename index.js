408af86578fa6bf202d79f411889dbc91184e9c3
Rob4t <robertgratza@web.de>
2020-12-14 17:28:47 +0100

    make inputs configurable, add input commands

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ var request = require("request");
 var Service, Characteristic, VolumeCharacteristic;
 const { exec } = require('child_process');
 
-const inputMap = {
-  1: "Hdmi1",
-  2: "Hdmi2",
-};
-
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
@@ -24,12 +19,15 @@ function CmdTelevisionAccessory(log, config) {
   this.name = config["name"];
   this.oncmd = config["oncmd"];
   this.offcmd = config["offcmd"];
-  this.input1cmd = config["input1cmd"];
-  this.input2cmd = config["input2cmd"];
+  this.statecmd = config["statecmd"];
+  this.stateok = config["stateok"];
+  this.inputMap = config["inputMap"];
+  this.interval = config["interval"];
 
   this.enabledServices = [];
 
-  this.isOn = false;
+  this.state = false;
+  this.activeIdent = 0;
 
   this.tvService = new Service.Television(this.name, "Television");
 
@@ -42,54 +40,112 @@ function CmdTelevisionAccessory(log, config) {
   this.tvService
     .getCharacteristic(Characteristic.Active)
     .on("set", this.setPowerState.bind(this))
-  this.tvService.setCharacteristic(Characteristic.ActiveIdentifier, 1);
+    .on("get", this.getPowerState.bind(this))
 
   this.tvService
     .getCharacteristic(Characteristic.ActiveIdentifier)
-    .on("set", this.setInput.bind(this, inputMap));
-
-  this.inputHDMI1Service = createInputSource("hdmi1", "HDMI 1", 1);
-  this.inputHDMI2Service = createInputSource("hdmi2", "HDMI 2", 2);
-
-  this.tvService.addLinkedService(this.inputHDMI1Service);
-  this.tvService.addLinkedService(this.inputHDMI2Service);
+    .on("set", this.setInput.bind(this))
+    .on("get", this.getInput.bind(this))
 
   this.enabledServices.push(this.tvService);
-  this.enabledServices.push(this.inputHDMI1Service);
-  this.enabledServices.push(this.inputHDMI2Service);
+  for (let key in this.inputMap) {
+    let source = this.inputMap[key]
+    let newInputService = createInputSource(source.name, source.label, key);
+    this.tvService.addLinkedService(newInputService);
+    this.enabledServices.push(newInputService);
+  }
+  this.statePolling()
 }
 
+//get input
+CmdTelevisionAccessory.prototype.getInput = function(callback) {
+  return callback(null, this.activeIdent);
+};
+
 //set input
-CmdTelevisionAccessory.prototype.setInput = function(map, newValue, callback) {
-  const remoteAction = map[newValue];
+CmdTelevisionAccessory.prototype.setInput = function(newValue, callback) {
+  const remoteAction = this.inputMap[newValue];
   if (!remoteAction) {
     callback(null);
   } else {
-      callback();
-      if (newValue == 1)
-            this.log("naar kanaal: 1");
-            exec(this.input1cmd);
-      if (newValue == 2)
-            this.log("naar kanaal: 2");
-            exec(this.input2cmd);
-    };
+    callback();
+    this.log("choose channel: "+newValue);
+    exec(this.inputMap[newValue].cmd);
+  };
 }
+
+// Method to determine current state
+CmdTelevisionAccessory.prototype.getState = function (callback) {
+  var self = this;
+
+  // Return cached state if no statecmd provided
+  if (this.statecmd === undefined) {
+    callback(null, this.state);
+    return;
+  }
+
+  // Execute command to detect state
+  exec(this.statecmd, function (error, stdout, stderr) {
+    var state = false;
+    var activeIdent = 0;
+
+    // Error detection
+    if (stderr) {
+      self.log("Failed to determine " + this.name + " state.");
+      self.log(stderr);
+    }
+    if (stdout) {
+      var ks = stdout.split("\n");
+      if (ks[0] === self.stateok) {
+        state = true
+      }
+      let foundIdent = 0
+      for (let key in self.inputMap) {
+        let source = self.inputMap[key]
+        if (source.identifier === ks[1]) {
+          activeIdent = key
+	}
+      }
+    }
+
+    self.log("State: " + state + " ident: " + activeIdent);
+    callback(stderr, state, activeIdent);
+  });
+}
+
+//state polling
+CmdTelevisionAccessory.prototype.statePolling = function() {
+  var self = this
+  clearTimeout(this.name)
+  this.getState(function (error, state, activeIdent) {
+    if (!error && state !== this.state) {
+      self.state = state
+      self.activeIdent = activeIdent
+      self.tvService.getCharacteristic(Characteristic.Active).getValue()
+      self.tvService.getCharacteristic(Characteristic.ActiveIdentifier).getValue();
+    }
+  })
+  setTimeout(this.statePolling.bind(this), this.interval * 1000)
+};
+
+//get power status
+CmdTelevisionAccessory.prototype.getPowerState = function(callback) {
+  return callback(null, this.state);
+};
 
 //set power status
 CmdTelevisionAccessory.prototype.setPowerState = function(state, callback) {
   this.log.debug("state", state);
   if (state) {
-    	exec(this.oncmd);
-      this.log("aangezet");
-      callback();
-    ;
+    exec(this.oncmd);
+    this.log("turn on");
+    callback();
   } else {
     exec(this.offcmd);
-	  this.log("uitgezet");
-      callback();
-    };
-  }
-;
+    this.log("turn off");
+    callback();
+  };
+};
 
 CmdTelevisionAccessory.prototype.getServices = function() {
   return this.enabledServices;
